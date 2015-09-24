48e226797604db68142270f10543f5a85c1ac483
Vladimir Motsak <vmotsak@gmail.com>
2015-09-24 22:23:48 +0200

    Add name to user on signup

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -12,6 +12,6 @@ feature 'Sign out', :devise do
     signin(user.email, user.password)
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
     click_link 'Sign out'
-    expect(page).to have_content I18n.t 'devise.sessions.signed_out'
+    expect(page).to have_content I18n.t 'devise.failure.unauthenticated'
   end
 end
