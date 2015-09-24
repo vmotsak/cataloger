847cb56a509167bb0986ddd77afff07ee1d4155b
Vladimir Motsak <vmotsak@gmail.com>
2015-09-24 22:24:08 +0200

    Add presence validation of product name

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   attachment :photo
+  validates_presence_of :name
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,3 +1,4 @@
 RSpec.describe Product, type: :model do
   it { is_expected.to belong_to :user }
+  it { is_expected.to validate_presence_of :name }
 end
