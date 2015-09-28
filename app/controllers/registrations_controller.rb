a5004490f6e9f7e91958ef0f4a64e39726725225
Vladimir Motsak <vmotsak@gmail.com>
2015-09-28 15:07:56 +0200

    Add Registrations controllers  - convert view to haml

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < ApplicationController
+  def admin
+  end
+
+  def owner
+  end
+
+  def visitor
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'registrations/admin'
+  get 'registrations/owner'
+  get 'registrations/visitor'
+
   resources :products
   devise_for :users
   root 'products#index'
